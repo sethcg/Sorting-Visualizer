a9fc6171e3d8c95336e13ce375aa625270799382
sethcg <sethcg@hotmail.com>
2025-10-21 04:24:19 -0400

    added window

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,112 @@
-/*
- * This example code creates an SDL window and renderer, and then clears the
- * window to a different color every frame, so you'll effectively get a window
- * that's smoothly fading between colors.
- *
- * This code is public domain. Feel free to use it for any purpose!
- */
+#define SDL_MAIN_USE_CALLBACKS 1 // USE CALLBACKS INSTEAD OF THE "main()" FUNCTION
 
-#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
+#include <stdlib.h>
+#include <time.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
 
-/* We will use this renderer to draw into this window every frame. */
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
 
-/* This function runs once at startup. */
+typedef struct AppContext {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    ImDrawData *data;
+} AppContext;
+
+// THIS FUNCTION RUNS ONCE AT STARTUP
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
-
-    if (!SDL_Init(SDL_INIT_VIDEO))
-    {
-        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0, &window, &renderer))
-    {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    // SETUP APP STATE
+    AppContext* appContext = (AppContext*) SDL_malloc(sizeof(AppContext));
+    if (appContext == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    *appstate = appContext;
+
+    if (!SDL_CreateWindowAndRenderer("Sorting Visualizer", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &appContext->window, &appContext->renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    // SETUP IMGUI
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.IniFilename = NULL;
+    io.LogFilename = NULL;
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplSDL3_InitForSDLRenderer(appContext->window, appContext->renderer);
+    ImGui_ImplSDLRenderer3_Init(appContext->renderer);
+
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-    if (event->type == SDL_EVENT_QUIT)
-    {
-        return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
+// THIS FUNCTION RUNS WHEN A NEW EVENT OCCURS (MOUSE INPUT, KEY PRESS, ETC.)
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
+    AppContext* appContext = (AppContext*) appstate;
+    
+    switch (event->type) {
+        case SDL_EVENT_QUIT:
+            return SDL_APP_SUCCESS;
     }
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+
+    ImGui_ImplSDL3_ProcessEvent(event);
+
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    const double now = ((double)SDL_GetTicks()) / 1000.0; /* convert from milliseconds to seconds. */
-    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
-    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
+// THIS FUNCTION RUNS ONCE PER FRAME
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    AppContext* appContext = (AppContext*) appstate;
 
-    /* clear the window to the draw color. */
-    SDL_RenderClear(renderer);
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
 
-    /* put the newly-cleared rendering on the screen. */
-    SDL_RenderPresent(renderer);
+    ImGui::SetNextWindowSize(ImVec2(136, 0));
+    ImGui::Begin("Control Panel", NULL, ImGuiWindowFlags_NoCollapse);
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    if (ImGui::Button("Sort", ImVec2(120, 20))) {
+        SDL_Log("Sorting...");
+    }
+
+    ImGui::End();
+
+    // RENDERING
+    ImGui::Render();
+
+    SDL_SetRenderDrawColor(appContext->renderer, 30, 30, 30, SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_RenderClear(appContext->renderer);
+
+    appContext->data = ImGui::GetDrawData();
+    ImGui_ImplSDLRenderer3_RenderDrawData(appContext->data, appContext->renderer);
+    SDL_RenderPresent(appContext->renderer);
+    
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once at shutdown. */
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
-    /* SDL will clean up the window/renderer for us. */
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    AppContext* appContext = (AppContext*) appstate;
+
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(appContext->renderer);
+    SDL_DestroyWindow(appContext->window);
+    SDL_Quit();
+
+    SDL_free(appContext);
 }
