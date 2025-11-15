bbcf770243eb4eb0c715ac198fdf11babe447615
sethcg <sethcg@hotmail.com>
2025-11-15 09:55:45 -0500

    improved color values

--- core/include/utils/Rect.hpp
+++ core/include/utils/Rect.hpp
@@ -10,9 +10,9 @@ typedef struct {
 #define rect_base_color rgb_color { .r = 50, .g = 50, .b = 50 }
 
 // OTHER COLORS TO BETTER DISTINGUISH THE SORTING PROCESS
-#define rect_green_color rgb_color { .r = 0, .g = 255, .b = 75 }
-#define rect_blue_color rgb_color { .r = 0, .g = 180, .b = 225 }
-#define rect_orange_color rgb_color { .r = 255, .g = 150, .b = 0 }
+#define rect_green_color rgb_color { .r = 0, .g = 150, .b = 75 }
+#define rect_blue_color rgb_color { .r = 0, .g = 120, .b = 160 }
+#define rect_orange_color rgb_color { .r = 200, .g = 120, .b = 0 }
 
 typedef struct {
     int value;
